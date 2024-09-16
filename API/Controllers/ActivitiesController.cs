4074550c961b6ac550d8ec7ef6c2ef59645efea3
Nick Chlam <nchlam@udr.com>
2024-09-16 16:40:42 -0600

    first commit

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,32 @@
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly ILogger<ActivitiesController> _logger;
+        private readonly DataContext _context;
+        public ActivitiesController(ILogger<ActivitiesController> logger, DataContext context)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            var activities = await _context.Activities.ToListAsync();
+            return activities; 
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            var activity = await _context.Activities.FindAsync(id);
+            return activity;
+        }
+    }
+}
